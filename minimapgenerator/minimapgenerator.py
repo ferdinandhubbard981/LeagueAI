0b6b77210c77d007fb82467559c2a2a136754f65
Ferdinand Hubbard <ferdinandhubbard@gmail.com>
2020-07-03 19:03:14 +0200

    added descriptions

--- minimapgenerator/minimapgenerator.py
+++ minimapgenerator/minimapgenerator.py
@@ -12,20 +12,23 @@ def placeimage(background, foreground, pos):
     return background
 
 def checkpos(pos, previouspos, touching):
+
     if touching == False:
+        #this will return false if any icon is touching another
         for oldpos in previouspos:
             if ((oldpos[0] - pos[0]) ** 2 + (oldpos[1] - pos[1]) ** 2) ** 0.5 < 24:
                 return False
         return True
     if touching == True:
+        #this will return false if any icon is not touching at least one other icon
         for oldpos in previouspos:
             if ((oldpos[0] - pos[0]) ** 2 + (oldpos[1] - pos[1]) ** 2) ** 0.5 > 24:
                 return False
         return True
 def makerandomminimap(touching, f, numofchamps):
 
-    images = listdir("croppedminimap/")
-    minimap = Image.open("croppedminimap/" + images[randint(0, len(images) - 1)])
+    images = listdir(minimapdir)
+    minimap = Image.open(minimapdir + images[randint(0, len(images) - 1)])
     #image.show()
     previouspos = []
 
@@ -44,25 +47,32 @@ def makerandomminimap(touching, f, numofchamps):
        champ = champs[object].strip("\n")
        #print(str(object) + " " + str((pos[0] + 12) / 256.0) + " " + str(((pos[1] + 12)) / 256.0) + " " + str(26 / 256.0) + " " + str(26 / 256.0) + "\n")
        f.write(str(object) + " " + str((pos[0] + 12) / 256.0) + " " + str(((pos[1] + 12)) / 256.0) + " " + str(26 / 256.0) + " " + str(26 / 256.0) + "\n")
-       championicon = Image.open("resizedtransparentchampionicons/" + champ + ".PNG")
+       championicon = Image.open(championicondir + champ.replace(" ", "_") + ".PNG")
        #print(champ + " pos: " + str(pos))
        minimap = placeimage(minimap, championicon, pos)
     return minimap
 
-f = open("lolchampsclean.txt", "r")
-champs = f.readlines()
-f.close()
+#variables to change for new datasets
+minimapdir = "newcroppedminimap/"
 subfolder = "data/"
 folder = "train/"
-maxnumofchamps = 10 #change
-numofimages = 100000 #change
+iconstouching = True
+maxnumofchamps = 10
+numofimages = 100000
+championicondir = "resizedtransparentchampionicons/"
+championlistdir = "lolchampsclean.txt"
+
+f = open(championlistdir, "r")
+champs = f.readlines()
+f.close()
+
 if __name__ == "__main__":
     offset = 1 #in case you interrupt, offset should be equal to the num of the last image (after "notouch" in its name)
     for i in range(numofimages):
         numofchamps = randint(1, maxnumofchamps)
         print(i + offset)
         f = open(subfolder + folder + "notouch" + str(i + offset) + ".txt", "w+")
-        minimap = makerandomminimap(False, f, numofchamps)
+        minimap = makerandomminimap(iconstouching, f, numofchamps)
         f.close()
         minimap.save(subfolder + folder + "notouch" + str(i + offset) + ".jpg")
         if i + offset >= numofimages * 0.9:
