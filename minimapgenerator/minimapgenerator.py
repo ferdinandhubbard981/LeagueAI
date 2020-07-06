52b840c72e0f0eb672d162e8ed0dd72b87893753
ferdinandhubbard981 <ferdinandhubbard@gmail.com>
2020-07-06 09:19:50 +0200

    Update minimapgenerator.py

--- minimapgenerator/minimapgenerator.py
+++ minimapgenerator/minimapgenerator.py
@@ -62,7 +62,7 @@ subfolder = "data/"
 folder = "train/"
 iconstouching = True
 maxnumofchamps = 10
-numofimages = 100000
+numofimages = 1000
 championicondir = "resizedtransparentchampionicons/"
 championlistdir = "lolchampsclean.txt"
 
